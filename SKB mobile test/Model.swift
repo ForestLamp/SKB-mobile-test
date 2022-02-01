c460aa2be11b28189ff5d5a9777234649bf7c9d5
ForestLamp <y100b@yandex.ru>
2022-02-01 15:12:03 +0500

    Initial commit

---
+++ SKB mobile test/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  SKB mobile test
+//
+//  Created by Alex Ch. on 01.02.2022.
+//
+
+import Foundation
+
+struct Model {
+    
+    let tableData = ["1","2","3","4","5","6","7"]
+    
+}
