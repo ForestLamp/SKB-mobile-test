36edb1a6b10af47fc5fb3e0eb8e4ac1dbffd96da
ForestLamp <y100b@yandex.ru>
2022-02-01 18:12:11 +0500

    Create custom Cell

---
+++ SKB mobile test/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  SKB mobile test
+//
+//  Created by Alex Ch. on 01.02.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var temperamentLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+
+}
--- SKB mobile test/MainScreen.swift
+++ SKB mobile test/MainScreen.swift
@@ -10,8 +10,13 @@ import UIKit
 class MainScreen: UITableViewController {
 
     @IBOutlet weak var navigationBarOutlet: UINavigationItem!
+  
     
     let tableData = ["1","2","3","4","5","6","7"]
+    
+    enum themperament: String {
+        case Melancholic, Phlegmatic, Sanguine, Choleric
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,53 +26,30 @@ class MainScreen: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return tableData.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
-
-        cell.textLabel?.text = tableData[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as! CustomTableViewCell
+//        cell.textLabel?.text = tableData[indexPath.row]
+//        cell.detailTextLabel?.text = tableData[indexPath.row]
+        cell.temperamentLabel.text = themperament.Choleric.rawValue
+        cell.phoneNumberLabel.text = "+7 922 039 23 68"
+        cell.nameLabel.text = "Alex Chusovitin"
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Hi"
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    // MARK: - TableWiew Delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -75,6 +57,4 @@ class MainScreen: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
